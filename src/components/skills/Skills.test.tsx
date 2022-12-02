6fbb4d43bfd6893c4195aaceac6a6fbb8b2d2607
jewoo <jeus0630@gmail.com>
2022-12-02 19:45:48 +0900

    Add Skills test

--- src/components/skills/Skills.test.tsx
+++ src/components/skills/Skills.test.tsx
@@ -16,4 +16,31 @@ describe("Skills", () => {
         expect(listItemElements).toHaveLength(skills.length);
     })
 
+    it("Should render login button", () => {
+        render(<Skills skills={skills}/>)
+        const loginButton = screen.getByRole('button',{
+            name: "Login"
+        });
+        expect(loginButton).toBeInTheDocument();
+    })
+
+    it("Should not render start learning button", () => {
+        render(<Skills skills={skills}/>)
+        const loginButton = screen.queryByRole('button',{
+            name: "Start learning"
+        });
+        expect(loginButton).not.toBeInTheDocument();
+    })
+
+    it("Should render start learning button eventually", async () => {
+        render(<Skills skills={skills}></Skills>);
+        const startLearningButton = await screen.findByRole("button",
+        {
+            name: "Start learning"
+        },
+        {
+            timeout: 2000
+        })
+        expect(startLearningButton).toBeInTheDocument();
+    })
 })
