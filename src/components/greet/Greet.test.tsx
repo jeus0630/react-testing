390cca1a02f58c2b27ca255f19649e5db945aea5
jewoo <jeus0630@gmail.com>
2022-12-02 11:14:55 +0900

    Add describe

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-it("Greet renders correctly", () => {
-    render(<Greet></Greet>);
-    const textElement = screen.getByText(/Hello/);
-    expect(textElement).toBeInTheDocument();
-})
-
-it("Greet renders with a name", () => {
-    render(<Greet name="Jewoo"></Greet>);
-    const textElement = screen.getByText(/Hello Jewoo/);
-    expect(textElement).toBeInTheDocument();
+describe("Greet",() => {
+    it("renders correctly", () => {
+        render(<Greet></Greet>);
+        const textElement = screen.getByText(/Hello/);
+        expect(textElement).toBeInTheDocument();
+    })
+    
+    it("renders with a name", () => {
+        render(<Greet name="Jewoo"></Greet>);
+        const textElement = screen.getByText(/Hello Jewoo/);
+        expect(textElement).toBeInTheDocument();
+    })
 })
