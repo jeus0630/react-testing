a3a19eab35354137a2e8f9b2ee95528cf79922a4
Jewoo <jeus0630@gmail.com>
2022-12-04 18:14:35 +0900

    Fix test error

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -6,7 +6,6 @@ describe('Greet', () => {
     render(<Greet></Greet>);
     const textElement = screen.getByText(/Hello/);
     expect(textElement).toBeInTheDocument();
-    expect(textElement).not.toBeInTheDocument();
   });
 
   it('renders with a name', () => {
